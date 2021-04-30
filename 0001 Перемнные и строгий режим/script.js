cef1cffd8ddd0e5df805b2f0017ec2aea3268f5e
InsanityJs <maks.naymenko@gmail.com>
2021-04-30 14:24:50 +0300

    First lesson complete

--- 0001 Перемнные и строгий режим/script.js
+++ 0001 Перемнные и строгий режим/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+let number = 5; //название переменной не может начинатся с цифры и спецсимвоал кроме($ и _)
+const leftBorderWidth = 1; //переменные обьявляют при помощью Camel case
+//название переменных зависят от регистра, то есть "number" и "Number" это разные переменные
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+}
+
+obj.a = 100;
+console.log(obj);
+// константу изменят нельзя, но по факту в js нету прямых констант и уже данные
+// константы в обьекте изменять можно, пример выше
+console.log(name);
+var name = 'Vasya';
+// проблема такой переменной, что var существует до того как обьявлен еще до начала кода
+
+{
+    let result = 50;
+    const task = false;
+    var line = 12;
+}
+console.log(line);
+console.log(result);
+console.log(task);
