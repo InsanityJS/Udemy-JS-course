52c190d601b79208b2ec64dd2b9556081be8dda1
InsanityJs <maks.naymenko@gmail.com>
2021-05-06 17:46:28 +0300

    Third lesson complete

---
+++ 0003 Простое общение с пользователем/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// alert('Hello') //* выскакивает модальное окно с предупреждением
+
+// const result = confirm('Are you here?'); //* модальное окно с подтверждением, 
+                                            //*может сохранять результат в переменную в виде булинового типа данных
+// console.log(result);
+
+// const answer = prompt('Вам есть 18', ''); //* Модальное окно с вопросом
+// console.log(answer); //* Ответ всегда в строковом типе данных сохраняется
+
+const answers = [];
+
+// answers[0] = prompt('Какое ваше имя?', '');
+// answers[1] = prompt('Какая у вас фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+console.log(typeof(answers));
