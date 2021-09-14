545c35995a3b5ebc7f5bdc95f5c772e803d82cda
InsanityJs <maks.naymenko@gmail.com>
2021-09-14 17:39:15 +0300

    Fiftynth lesson complete

---
+++ 0015 Массивы и псевдомассивы/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+const arr = [1, 2, 3, 5, 8];
+// arr[99] = 48;
+// console.log(arr.length);  //* показывает длинну массива, но показывает ее так индекс+1
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+}) //* Перебор массива с помощью forEach
+
+// arr.pop(); //* удаляет последнее значение в массиве
+// arr.push(12); //* добавляет в конец массива значение
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) { //* Цикл который перебирает массив
+//     console.log(arr[i]);
+// }
+
+for (let  value of arr) { //* for of - еще один метод переборки массива, такой метод работает только с массивами
+    console.log(value);
+}
+
+const str = prompt('','');
+const products = str.split(', '); //* Метод split позволяет сформировать массив из строк отфильтровывая его через любой знак в условии
+// console.log(products);
+products.sort();
+console.log(products.join(";")); //*метод join позвоялет из массива сделать строку с разделителем, который будет в условии
