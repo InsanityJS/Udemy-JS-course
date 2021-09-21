e8d2321d4fda4d1a116f9ab5d2bc686445948c3d
Maks <maks.naymenko@gmail.com>
2021-09-21 21:58:27 +0300

    First project practice complete

--- project/js/script.js
+++ project/js/script.js
@@ -12,15 +12,41 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+"use strict";
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+    movies: ["Логан", "Лига справедливости", "Ла-ла лэнд", "Одержимость", "Скотт Пилигрим против..."],
 };
+//* Переменные с данными сайта
+const advertising = document.querySelector(".promo__adv"),
+    adverItem = advertising.querySelectorAll("img"),
+    posterBG = document.querySelector(".promo__bg"),
+    genre = posterBG.querySelector(".promo__genre"),
+    movieList = document.querySelector(".promo__interactive-list");
 
+//* Первое задание
+
+//advertising.remove();
+adverItem.forEach((item) => {
+    item.remove();
+});
+//* Второе задание
+
+genre.textContent = "драма";
+
+//* Третье задание
+
+posterBG.style.backgroundImage = "url(img/bg.jpg)";
+
+//* Четвертое задание
+
+movieList.innerHTML = "";
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+         <div class="delete"></div>
+    </li>   
+    `;
+});
